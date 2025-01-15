06b14aad1fed8c833ffa5649c8a71f08a27be144
batzim <rikelmybarboza@gmail.com>
2025-01-14 23:27:12 -0300

    Modifiquei para que seja uma array 👌

--- ITP PARTE 3/projeto/gerar_code.cpp
+++ ITP PARTE 3/projeto/gerar_code.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime> // Biblioteca para manipulação de tempo
 using namespace std;
 
 int main() {
     srand(time(0)); //Gera um número aleatório a partir do tempo atual (biblioteca cstdlib)
 
+    int sete_digitos[7]; // Armazena os 7 primeiros digitos
+    int soma = 0; // Armazena a soma dos 7 primeiros digitos
+
     for (int i = 0; i < 7; i++) {
-        int sete_digitos = rand() % 10; //Gera um número aleatório entre 0 e 9
-        cout << sete_digitos << endl; //Imprime o número gerado
+        sete_digitos[i] = rand() % 10; //Gera um número aleatório entre 0 e 9
+        cout << sete_digitos[i] << endl; //Imprime o número gerado
     }
 
-    return 0;
+    cout << endl; 
 }
+
