cbd4dc990ae9d5a01a80faea1df08e52b3a65af2
batzim <rikelmybarboza@gmail.com>
2025-01-17 11:26:09 -0300

    codigo aleatorio finalizado (acho)

--- ITP PARTE 3/projeto/gerar_code.cpp
+++ ITP PARTE 3/projeto/gerar_code.cpp
@@ -4,30 +4,41 @@
 using namespace std;
 
 int main() {
-    srand(time(0)); //Gera um número aleatório a partir do tempo atual (biblioteca cstdlib)
-
-    int sete_digitos[7]; // Armazena os 7 primeiros digitos
     int soma = 0; // Armazena a soma dos 7 primeiros digitos
+    int sete_digitos[7]; // Armazena os 7 primeiros digitos
+    int oitavo_digito; // Armazena o oitavo digito
 
-    for (int i = 0; i < 7; i++) {
-        sete_digitos[i] = rand() % 10; //Gera um número aleatório entre 0 e 9
-        cout << sete_digitos[i] << endl; //Imprime o número gerado
-    }
+    do {
+        srand(time(0)); //Gera um número aleatório a partir do tempo atual (biblioteca cstdlib)
 
-    cout << endl; // remover dps
-
-
-// Calcula a soma com os multiplicadores alternados entre 3 e 1
-    for (int i = 0; i < 7; i++) {
-        if (i % 2 == 0) { // Índices pares usam multiplicador 3
-            soma += 3 * sete_digitos[i];
-        } else { // Índices ímpares usam multiplicador 1
-            soma += sete_digitos[i];
+        for (int i = 0; i < 7; i++) {
+            sete_digitos[i] = rand() % 10; //Gera um número aleatório entre 0 e 9
         }
-    }
 
-    // só pra saber se ta certinho (temporario)
-    cout << "Soma calculada: " << soma << endl;
+        // Calcula a soma com os multiplicadores alternados entre 3 e 1
+        soma = 0; // Reset soma
+        for (int i = 0; i < 7; i++) {
+            if (i % 2 == 0) { // Índices pares usam multiplicador 3
+                soma += 3 * sete_digitos[i];
+            } else { // Índices ímpares usam multiplicador 1
+                soma += sete_digitos[i];
+            }
+        }
+
+    } while (soma % 10 == 0);
+
+cout << endl; // remover dps
+
+// Subtrai o oitavo digito e soma com os sete_digitos
+
+    oitavo_digito = 10 - (soma % 10); // Calcula o oitavo digito
+
+    // Imprime o código gerado
+    cout << "Código gerado: ";
+    for (int i = 0; i < 7; i++) {
+        cout << sete_digitos[i]; // Imprime os 7 primeiros digitos
+    }
+    cout << oitavo_digito << endl; // Imprime o oitavo digito formatado
 
     return 0;
 }
